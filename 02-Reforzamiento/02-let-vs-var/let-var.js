c9a6869e4b92fde6ed4a61c6b0173e3322b61fec
Ale1120 <alejandro.ortegano11@gmail.com>
2019-05-07 12:55:22 -0400

    difference between variable var and let

---
+++ 02-Reforzamiento/02-let-vs-var/let-var.js
@@ -0,0 +1,17 @@
+// puede ser cambiar el valor de la varible
+var nombre = "Wolverine";
+
+if (true){
+  var nombre = "Mageneto"
+}
+
+console.log(nombre);
+
+// no puede ser redefinida
+let nombre1 = "Wolverine";
+// solo de una forma nombre1 = "Fenix"
+if (true){
+  let nombre1 = "Mageneto"
+}
+
+console.log(nombre1);
