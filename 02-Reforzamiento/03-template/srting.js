beb9d930eefacd1c44c42a35ada6b5a0df5ac5ad
Ale1120 <alejandro.ortegano11@gmail.com>
2019-05-07 13:06:33 -0400

    learning template string in node.js

--- 02-Reforzamiento/03-template/srting.js
+++ 02-Reforzamiento/03-template/srting.js
@@ -1,5 +1,12 @@
-let nombre = "Deadpool"
-let nombreReal = "Wade Winston"
+let nombre = "Deadpool";
+let nombreReal = "Wade Winston";
 
-console.log(nombre + ' ' + nombreReal)
-console.log(`${ nombre } ${ nombreReal }`)
+console.log(nombre + ' ' + nombreReal);
+console.log(`${ nombre } ${ nombreReal }`);
+
+
+function getNombre(){
+  return `${ nombre } ${ nombreReal }`;
+}
+
+console.log(`El nombre de: ${ getNombre() } es de una funcion`);
