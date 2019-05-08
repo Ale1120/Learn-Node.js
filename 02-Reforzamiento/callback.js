afb531d7a9ad6b5808b1331665efb938b69f8da4
Ale1120 <alejandro.ortegano11@gmail.com>
2019-05-08 13:17:17 -0400

    learnig callback in node.js

--- 02-Reforzamiento/callback.js
+++ 02-Reforzamiento/callback.js
@@ -1,5 +1,26 @@
 // Calllback
 
 setTimeout( () => {
-  conole.log("Hola mundo")
+  console.log("Hola mundo")
 }, 3000);
+
+let getUsuarioById = (id, calllback ) => {
+  let usuario = {
+    nombre: 'Alejandro',
+    id  // id: id
+  }
+  if (id === 20){
+    calllback(`El usuario con id ${id}, no existe en la DB`);
+  } else {
+      calllback(null, usuario);
+  }
+}
+
+
+
+getUsuarioById(2, (err, usuario)=> {
+  if (err ){
+    return console.log(err);
+  }
+  console.log("Usuario de base de datos", usuario)
+});
