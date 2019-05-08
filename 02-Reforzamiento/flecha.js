a68998cc6871edc6eede787ec5058fe59964f867
Ale1120 <alejandro.ortegano11@gmail.com>
2019-05-08 12:47:12 -0400

    function in object node.js

--- 02-Reforzamiento/flecha.js
+++ 02-Reforzamiento/flecha.js
@@ -7,3 +7,23 @@
 let sumar = (a,b) => a + b;
 
 console.log(sumar(10,20));
+
+
+// ejeccio 1
+// let saludar = () => 'hola mundo';
+
+let saludar = nombre => `Hola ${ nombre }`;
+
+console.log(saludar("Alejandro"));
+
+
+let deadpool = {
+  nombre: "Wade",
+  apellido: "Winston",
+  poder: "Regeneracion",
+  getNombre() {
+    return ` ${ this.nombre} ${ this.apellido } - poder: ${ this.poder} `
+  }
+}
+
+console.log(deadpool.getNombre());
