b869584c0f47fcbf8443d38b4d1cb64a747b1025
Ale1120 <alejandro.ortegano11@gmail.com>
2019-05-08 13:20:48 -0400

    create callback2 and objects

---
+++ 02-Reforzamiento/callback2.js
@@ -0,0 +1,18 @@
+let empleados = [{
+  id: 1,
+  nombre: "Alejandro"
+},{
+  id: 2,
+  nombre: "Anthony"
+},{
+  id: 3,
+  nombre: "Cesar"
+}];
+
+let salario = [{
+  id: 1,
+  salario: 1000
+},{
+  id: 2,
+  salario: 2000
+}];
