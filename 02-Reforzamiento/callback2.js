d734eab499ed0e2c321b336828c8419b1fd4418d
Ale1120 <alejandro.ortegano11@gmail.com>
2019-05-08 14:22:25 -0400

    callback in chain

--- 02-Reforzamiento/callback2.js
+++ 02-Reforzamiento/callback2.js
@@ -9,10 +9,56 @@ let empleados = [{
   nombre: "Cesar"
 }];
 
-let salario = [{
+let salarios = [{
   id: 1,
   salario: 1000
 },{
   id: 2,
   salario: 2000
 }];
+
+
+let getSalario = (empleado, callback) => {
+
+  let salarioDB = salarios.find( salario => salario.id == empleado.id);
+
+  if( !salarioDB) {
+    callback(` No se encontro un salario para el empleado ${ empleado.nombre }`);
+  } else {
+    callback(null,{
+      nombre: empleado.nombre,
+      salario: salarioDB.salario,
+      id: empleado.id
+    });
+  }
+
+}
+
+
+let getEmpleado = (id, calllback ) => {
+  let empleadoDB = empleados.find( empleado => empleado.id == id)
+
+  if ( !empleadoDB ){
+    calllback(` No existe un empleado con el ID ${ id}`);
+  } else {
+      calllback(null, empleadoDB);
+  }
+}
+
+getEmpleado(3, (err, empleado) => {
+
+  if (err){
+    return console.log(err);
+  }
+
+  getSalario(empleado, (err, resp) => {
+
+    if (err){
+      return console.log(err);
+    }
+    console.log(`El Salario de ${ resp.nombre} es de ${ resp.salario }$`);
+
+  });
+
+
+});
